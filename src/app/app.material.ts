f2d9dd68c25211a14fb818e4a0901c7dd5a3cd2b
Parvat1.Singh <Parvat1.Singh@ril.com>
2023-02-14 10:25:19 +0530

    added material.ts to manage material module

---
+++ src/app/app.material.ts
@@ -0,0 +1,13 @@
+// This file maintain the all the module from angular material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+export const materialArray = [
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatDividerModule,
+  MatListModule,
+];
